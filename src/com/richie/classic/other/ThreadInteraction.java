8be9a4f021607b3215251e5a7e9e6c75b597673f
Richie <isuperqiang@gmail.com>
2019-04-07 17:49:17 +0800

    java wait and condition

---
+++ src/com/richie/classic/other/ThreadInteraction.java
@@ -0,0 +1,15 @@
+package com.richie.classic.other;
+
+/**
+ * @author richie on 2018.06.22
+ */
+public class ThreadInteraction {
+
+    public static void main(String[] args) {
+
+        //new ConditionDemo().start();
+        //new WaitNotifyTest().start();
+        //new ConditionDemo().printNumber();
+        new WaitNotifyTest().printNumber();
+    }
+}
