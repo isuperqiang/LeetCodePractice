41db9e47638b29dbc6c58fa06181bbf100a7dea2
Richie <isuperqiang@gmail.com>
2019-07-20 16:50:12 +0800

    乘积最大子序列

---
+++ src/com/richie/leetcode/medium/Medium_152.java
@@ -0,0 +1,82 @@
+package com.richie.leetcode.medium;
+
+/**
+ * @author Richie on 2019.07.20
+ */
+public class Medium_152 {
+    /**
+     * 乘积最大子序列
+     *
+     * <p>
+     * 地址：https://leetcode-cn.com/problems/maximum-product-subarray/
+     * </p>
+     *
+     * <p>
+     * 描述：给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。
+     * </p>
+     *
+     * <p>
+     * 示例：
+     * 输入: [2,3,-2,4]
+     * 输出: 6
+     * 解释: 子数组 [2,3] 有最大乘积 6。
+     * </p>
+     *
+     * <p>
+     * 解答：
+     * - 解法一：暴力解法，遍历数组，逐个求出连续子序列的乘积，找到最大值。时间复杂度 O(n^2)。
+     * - 解法二：动态规划，遍历数组，计算当前最大值，并不断更新。当前最大乘积 max，当前最小乘积 min。由于存在负数，会导致最大的变成最小的，
+     * 最小的变成最大的。所以分两种情况讨论：
+     * - nums[i] > 0 时，max = Max(max * nums[i], nums[i]); min = Min(min * num[i], num[i]);
+     * - nums[i] < 0 时，max = Max(min * nums[i], nums[i]); min = Min(max * num[i], num[i]);
+     * </p>
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        int i = maxProduct2(nums);
+        System.out.println(i); // 6
+    }
+
+    private static int maxProduct1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0];
+        int temp;
+        for (int i = 0; i < nums.length; i++) {
+            temp = nums[i];
+            if (temp > max) {
+                max = temp;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                temp *= nums[j];
+                if (temp > max) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int maxProduct2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int ret = nums[0];
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            max = Math.max(max * nums[i], nums[i]);
+            min = Math.min(min * nums[i], nums[i]);
+            ret = Math.max(ret, max);
+        }
+        return ret;
+    }
+
+}
