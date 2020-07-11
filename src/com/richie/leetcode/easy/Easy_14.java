58e48637b5bf5bbc7d2264b021f062c4af1c6973
RichieLiu <isuperqiang@gmail.com>
2020-07-11 15:35:47 +0800

    refactor code

--- src/com/richie/leetcode/easy/Easy_14.java
+++ src/com/richie/leetcode/easy/Easy_14.java
@@ -8,24 +8,35 @@ public class Easy_14 {
      * 最长公共前缀
      *
      * <p>
-     * 地址：https://leetcode-cn.com/problems/longest-common-prefix/description/
+     * 地址：https://leetcode-cn.com/problems/longest-common-prefix/
      * </p>
      *
      * <p>
      * 描述：编写一个函数来查找字符串数组中的最长公共前缀。
      * 如果不存在公共前缀，返回空字符串 ""。
+     * 所有输入只包含小写字母 a-z 。
+     * </p>
+     *
+     * <p>
+     * 示例：
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
      * </p>
      *
      * <p>
      * 解答：首先确定字符数组中最短的字符串长度，然后遍历字符串，逐个比较相同索引位置的字符是否相同，从而找到公共前缀
+     * </p>
+     *
+     * <p>
+     * 时间复杂度：O(mn)
+     * 空间复杂度：O(1)
      * </p>
      */
 
     public static void main(String[] args) {
         String[] s = {"flower", "flow", "flight"};
         String ret = longestCommonPrefix(s);
-        System.out.println(ret);
-        // fl
+        System.out.println(ret); // fl
     }
 
     private static String longestCommonPrefix(String[] strs) {
@@ -37,17 +48,16 @@ public class Easy_14 {
         for (String str : strs) {
             minLen = Math.min(str.length(), minLen);
         }
-        int length = strs.length;
         int index = 0;
         String first = strs[0];
         for (int i = 0; i < minLen; i++) {
-            for (int j = 1; j < length; j++) {
+            for (int j = 1; j < strs.length; j++) {
                 if (strs[j].charAt(i) != first.charAt(i)) {
                     return first.substring(0, index);
                 }
             }
             index++;
         }
-        return first.substring(0, index);
+        return first;
     }
 }
--- src/com/richie/leetcode/easy/Easy_344.java
+++ src/com/richie/leetcode/easy/Easy_344.java
@@ -1,5 +1,7 @@
 package com.richie.leetcode.easy;
 
+import java.util.Arrays;
+
 /**
  * @author richie on 2018.05.29
  */
@@ -9,37 +11,47 @@ public class Easy_344 {
      * 反转字符串
      *
      * <P>
-     * 地址：https://leetcode-cn.com/problems/reverse-string/description/
+     * 地址：https://leetcode-cn.com/problems/reverse-string/
      * </P>
      *
      * <p>
-     * 描述：请编写一个函数，其功能是将输入的字符串反转过来。
+     * 描述：请编写一个函数，其功能是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     * </p>
+     *
+     * <p>
+     * 示例：
+     * 输入：["h","e","l","l","o"]
+     * 输出：["o","l","l","e","h"]
      * </p>
      *
      * <p>
      * 解答：遍历字符数组，然后依次交换前后的位置
      * </p>
+     *
+     * <p>
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * </p>
      */
 
     public static void main(String[] args) {
-        String s = "hello";
-        String re = reverseString(s);
-        System.out.println(re);
-        // 输出：olleh
+        char[] chars = new char[]{'h', 'e', 'l', 'l', 'o'};
+        reverseString(chars);
+        System.out.println(Arrays.toString(chars));
     }
 
-    private static String reverseString(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
+    private static void reverseString(char[] chars) {
+        if (chars == null || chars.length == 0) {
+            return;
         }
-
-        char[] chars = s.toCharArray();
         int length = chars.length;
-        for (int i = 0; i < length / 2; i++) {
+        for (int i = 0, half = length / 2; i < half; i++) {
             char temp = chars[i];
             chars[i] = chars[length - i - 1];
             chars[length - i - 1] = temp;
         }
-        return new String(chars);
     }
+
 }
