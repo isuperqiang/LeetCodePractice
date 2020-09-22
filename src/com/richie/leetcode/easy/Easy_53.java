3b0034414b21063c727f0690173135b9c56c9863
liuqiang <qiangliu@faceunity.com>
2020-09-22 09:59:32 +0800

    Easy_53 maxSubArray

--- src/com/richie/leetcode/easy/Easy_53.java
+++ src/com/richie/leetcode/easy/Easy_53.java
@@ -8,29 +8,48 @@ public class Easy_53 {
      * 最大子序和
      *
      * <p>
-     * 地址：https://leetcode-cn.com/problems/maximum-subarray/description/
+     * 地址：https://leetcode-cn.com/problems/maximum-subarray/
      * </p>
      *
      * <p>
-     * 描述：给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * 描述：
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * </p>
+     *
      * <p>
      * 示例:
-     * <p>
      * 输入: [-2,1,-3,4,-1,2,1,-5,4],
      * 输出: 6
      * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
      * </p>
      *
      * <p>
-     * 解答：求和，然后判断和是否小于0。因为只要前面的和小于0，那么后面的数加上前面的和就一定比自身小，所以又重新求和，并和之前的最大子序和比较，取最大值。
+     * 解答：
+     * - 贪心算法
+     * 1. 遍历数组，当前最大子序和 sum，返回值 max
+     * 2. 如果 sum<0，其对子序和的增加没有正面效果，需要舍弃，重置 sum 为 0,
+     * 3. sum 加上当前值，并比较和 max 的大小，记录 max
+     * - 动态规划
+     * 1. 定义状态，dp[i] 表示从 0 到 i 的数组中的最大子序和
+     * 2. 初始状态，dp[0] 就是第一个元素值 nums[0]
+     * 3. 状态转移方程，如果 dp[i-1]<0，dp[i-1]+nums[i] 肯定小于 nums[i]，这时就要从 nums[i] 开始计数
+     * 4. 比较每步的 dp[i]，保存到 max，最后返回结果
+     * </p>
+     *
+     * <p>
+     * - 贪心算法：
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * - 动态规划
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
      * </p>
      */
 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int max = maxSubArray(nums);
-        System.out.println(max);
-        // 6
+        System.out.println(max); // 6
     }
 
     private static int maxSubArray(int[] nums) {
@@ -38,15 +57,30 @@ public class Easy_53 {
             return 0;
         }
         int sum = 0;
-        int max = Integer.MIN_VALUE;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
+        int max = nums[0];
+        for (int num : nums) {
             if (sum < 0) {
                 sum = 0;
             }
-            sum += nums[i];
-            max = Math.max(sum, max);
+            sum += num;
+            max = Math.max(max, sum);
         }
         return max;
     }
+
+    private static int maxSubArray2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
 }
